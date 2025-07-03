097b600f5d61e6095ff9648f7845511d22a1a2fd
Pranay Jain <jainpranay20@gmail.com>
2025-07-03 18:45:29 +0530

    clerk middleware added

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Header from "@/components/ui/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Medimeet - Doctors Appointment App",
@@ -12,27 +14,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className}`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header/>
-                  <main className="min-h-screen">
-          {children}
-        </main>
-        <footer className="bg-muted/50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-200">
-            <p>Made by Pranay</p>
-          </div>
-        </footer>
-
-        </ThemeProvider>
-
-      </body>
-    </html>
+    <ClerkProvider 
+      appearance={{
+        baseTheme: dark,
+      }}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made by Pranay</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body> 
+      </html>
+    </ClerkProvider>
   );
 }
--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,23 +1,54 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./button";
 
 const Header = () => {
   return (
-    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-[backdrop-filter]:bg-background/60'>
-        <nav className='container mx-auto px-4 h-16 flex-center justify-between'>
-            <Link href="/">
-                <Image
-                src='/logo-single.png'
-                alt="Medimeet Logo"
-                width={200}
-                height={60}
-                className="h-20 w-auto object-contain"
-                />
-            </Link>
-        </nav>
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo-single.png"
+            alt="Medimeet Logo"
+            width={200}
+            height={60}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-center space-x-2">
+          <SignedOut>
+              <SignInButton>
+                <Button variant="secondary">Sign In</Button>
+              </SignInButton>
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton 
+                appearance={{
+                  elements: {
+                    avatarBox: "w-10 h-10",
+                    userButtonPopoverCard: "shadow-xl",
+                    userPreviewMainIdentifier: "font-semibold"
+                  }
+                }}
+              />
+            </SignedIn>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
